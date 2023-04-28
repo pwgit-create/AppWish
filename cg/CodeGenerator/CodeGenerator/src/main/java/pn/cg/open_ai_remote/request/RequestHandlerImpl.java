4dd12e651a221c50ce1f37bbc5ed8b34f820603b
snow901 <snow_900@outlook.com>
2023-04-28 15:34:20 +0200

    Updated deprecated code

--- cg/CodeGenerator/CodeGenerator/src/main/java/pn/cg/open_ai_remote/request/RequestHandlerImpl.java
+++ cg/CodeGenerator/CodeGenerator/src/main/java/pn/cg/open_ai_remote/request/RequestHandlerImpl.java
@@ -1,8 +1,9 @@
 package pn.cg.open_ai_remote.request;
 
-import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.OpenAiApi;
 import com.theokanning.openai.completion.CompletionChoice;
 import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pn.cg.datastorage.constant.CodeGeneratorConstants;
@@ -11,7 +12,7 @@ import java.util.List;
 
 public class RequestHandlerImpl implements RequestHandler {
 
-    private final OpenAiService service;
+    private final com.theokanning.openai.service.OpenAiService service;
 
     private static final Logger log = LoggerFactory.getLogger(RequestHandlerImpl.class);
 
